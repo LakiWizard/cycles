b88b081a60e2f24e96bf2d4a63007cdc5c438edb
laki <>
2018-10-06 13:41:56 +0200

    ongoing work
    the players are now moving, and cannot exit map boundaries. so far so good

--- cycles.py
+++ cycles.py
@@ -2,6 +2,7 @@
 import pygame
 
 
+all_events = None
 
 def initialize(screen_width, screen_height):
     pygame.init()
@@ -15,6 +16,17 @@ def initialize(screen_width, screen_height):
 def deinitialize():
     pygame.quit()
 
+
+
+p1_input = {"left": pygame.K_a,
+            "right": pygame.K_d,
+            "up": pygame.K_w,
+            "down": pygame.K_s}
+
+p2_input = {"left": pygame.K_LEFT,
+            "right": pygame.K_RIGHT,
+            "up": pygame.K_UP,
+            "down": pygame.K_DOWN}
 
 
 class GameMap():
@@ -34,8 +46,49 @@ class Player():
         self.y = y
         self.color = color  # pygame.Color object
 
+        self.direction = "up"
+        self.input_dict = None
+
         # hold a reference to the map you are on
         self.game_map = game_map
+
+    def handle_input(self):
+        global all_events
+
+        valid_input = self.input_dict.values()
+        user_input = None
+
+        for e in all_events:
+            if e.type == pygame.KEYDOWN:
+                if e.key in valid_input:
+                    user_input = e.key
+        if user_input is None:
+            return None
+
+        reversed_dict = {value: key for key, value in self.input_dict.items()}
+        new_direction = reversed_dict[user_input]
+
+        self.direction = new_direction
+
+    def update(self):
+        status_good = "moving"
+        status_crashed = "crashed"
+
+        if self.x < 0 or self.x > self.game_map.width or self.y < 0 or self.y > self.game_map.height:
+            return status_crashed
+
+        if self.direction == "up":
+            self.y -= 1
+            return status_good
+        elif self.direction == "down":
+            self.y += 1
+            return status_good
+        elif self.direction == "left":
+            self.x -= 1
+            return status_good
+        elif self.direction == "right":
+            self.x += 1
+            return status_good
 
     def draw(self, screen_surface):
         pygame.draw.circle(screen_surface, self.color, (self.x, self.y), 10, 4)
@@ -52,6 +105,8 @@ def draw_all(game_map, player_list, screen_surface):
 
 
 def main():
+    global all_events
+
     map_size = (800, 600)
 
     game_map = GameMap(*map_size)
@@ -60,7 +115,9 @@ def main():
     red = pygame.color.Color(220, 50, 50)
 
     p1 = Player(100, 50, blue, game_map)
+    p1.input_dict = p1_input
     p2 = Player(500, 350, red, game_map)
+    p2.input_dict = p2_input
     players = [p1, p2]
 
     screen_surface = initialize(*map_size)
@@ -69,12 +126,19 @@ def main():
 
     game_running = True
     while game_running:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    game_running = False
+        all_events = pygame.event.get()
+
+        for e in all_events:
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+                game_running = False
+
+        p1.handle_input()
+        p2.handle_input()
+        p1.update()
+        p2.update()
 
         draw_all(game_map, players, screen_surface)
-        clock.tick(30)
+        clock.tick(60)
 
     deinitialize()
 
