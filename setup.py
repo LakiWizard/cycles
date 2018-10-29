03ec175dbdc2718ec6897a7a6e7a6a1b23495991
LakiWizard <lakidevgh@gmail.com>
2018-10-29 10:24:35 +0100

    added setup.py for cx_freeze
    added setup.py file for building windows .exe files using cx_freeze

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+import sys
+from cx_Freeze import setup, Executable
+
+
+base = None
+# if on win, dont show console
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+# include the font file
+to_include = ["DejaVuSansMono.ttf"]
+
+options = {
+    "build_exe": {
+        "include_files": to_include
+        }
+}
+
+setup(name="Cycles",
+      version="0.9",
+      executables=[Executable("cycles.py", base=base)],
+      options=options
+      )
